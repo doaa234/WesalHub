7215eb5fcb740f9b6a619d6da233b752debddac7
doaa galal <doaagalal08@gmail.com>
2025-05-03 23:11:19 +0300

    your commit message

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Server is running!");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running!');
+});
